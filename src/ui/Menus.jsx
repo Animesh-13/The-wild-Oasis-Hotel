47b032d6fff062041f5a60f09607dacab55d581b
aks <animeshkumarsrivastava13@gmail.com>
2023-08-14 22:09:11 +0530

    project completed....

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -86,6 +86,7 @@ function Toggle({ id }) {
   const { openId, close, open, setPosition } = useContext(MenusContext);
 
   function handleClick(e) {
+    e.stopPropagation();
     const rect = e.target.closest("button").getBoundingClientRect();
     // console.log(rect);
     setPosition({
@@ -104,7 +105,7 @@ function Toggle({ id }) {
 
 function List({ id, children }) {
   const { openId, position, close } = useContext(MenusContext);
-  const ref = useOutsideClick(close);
+  const ref = useOutsideClick(close, false);
 
   if (openId !== id) return null;
 
